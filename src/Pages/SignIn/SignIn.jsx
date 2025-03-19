836165a78402c23ba6fbcdc447671099116544eb
ZakariaZack98 <sadeemdzakaria@yahoo.com>
2025-03-19 15:43:04 +0600

    google popup login implemented

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { FcGoogle } from 'react-icons/fc'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { toast } from 'react-toastify';
 
 const SignIn = () => {
   const auth = getAuth();
+
   //setting up states
   const [loginInfo, setLoginInfo] = useState({email: '', password: ''});
 
-  //input change handler
+  //input change handler================================================
   const handleChange = e => {
     const {name, value} = e.target;
     setLoginInfo({
@@ -17,9 +18,39 @@ const SignIn = () => {
     })
   }
 
-  //submit handler
+  //google login handler=================================================
   /**
-   * todo: to handle login
+   * TODO: HANDLE SIGN IN VIA GOOGLE
+   * @param null
+   * return: void
+   * **/
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+    .then(credInfo => {
+      const cred = GoogleAuthProvider.credentialFromResult(credInfo);
+      console.log(cred);
+      const token = cred.accessToken;
+      const user = credInfo.user;
+      toast.success(`Welcome back, ${user.displayName}`);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      toast.error('Login failed', errorMessage);
+    });
+  }
+
+
+
+  //submit handler==================================================
+  /**
+   * TODO: HANDLE SIGN IN WITH EMAIL & PASSWORD
    * @param loginInfo
    * return: void 
    *  **/
@@ -40,7 +71,7 @@ const SignIn = () => {
       <div className="signInLeft h-full w-1/2 flex justify-center items-center">
         <div className="signUpLeftComp flex flex-col justify-center items-start">
           <h1 className="text-[34px] font-bold text-authFontColor">Login to your account</h1>
-          <div className='flex gap-x-3 border-gray-200 border-1 border-solid rounded-2xl px-15 py-4 my-5 cursor-pointer'>
+          <div className='flex gap-x-3 border-gray-200 border-1 border-solid rounded-2xl px-15 py-4 my-5 cursor-pointer' onClick={handleGoogleLogin}>
             <span className='text-2xl'><FcGoogle/></span>
             <p className='text-sm text-authFontColor'>Login with google</p>
           </div>
