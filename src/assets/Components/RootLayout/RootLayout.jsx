94e9e79c3464b70915a3b6e4a1b5c26515edb9f6
ZakariaZack98 <sadeemdzakaria@yahoo.com>
2025-03-25 22:27:22 +0600

    homepage authorization deployed

--- src/assets/Components/RootLayout/RootLayout.jsx
+++ src/assets/Components/RootLayout/RootLayout.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../HomeComponents/Sidebar'
 import { Outlet } from 'react-router-dom'
+import { getAuth } from 'firebase/auth'
+import Error from '../../../Pages/Error/Error'
 
 const RootLayout = () => {
-  return (
-    <div className='rootlayout bg-cover bg-center bg-no-repeat h-svh px-20 py-5 flex gap-x-5'>
-      <Sidebar/>
-      <div className="mainBody w-full ">
-        <Outlet/>
+  const auth = getAuth();
+  const [userAuthorized, setUserAuthorized] = useState(false);
+
+  useEffect(() => {
+    auth.currentUser && auth.currentUser.emailVerified ? setUserAuthorized(true) : setUserAuthorized(false);
+  })
+
+  if(userAuthorized) {
+    return (
+      <div className='rootlayout bg-cover bg-center bg-no-repeat h-svh px-20 py-5 flex gap-x-5'>
+        <Sidebar/>
+        <div className="mainBody w-full ">
+          <Outlet/>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else return <Error/>
 }
 
 export default RootLayout
