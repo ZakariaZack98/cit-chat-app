749ebb81bec265c414a6e5858a4c20e829c878b5
ZakariaZack98 <sadeemdzakaria@yahoo.com>
2025-04-24 20:24:53 +0600

    react emoji picker deployed

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,19 +1,24 @@
 import React, { useContext, useState } from "react";
 import GroupList from "../../assets/Components/HomeComponents/GroupList";
 import Friends from "../../assets/Components/HomeComponents/Friends";
-import { activeConvUserData, getChatData } from "../../lib/componentsData";
+import { getChatData } from "../../lib/componentsData";
 import UserPart from "../../assets/Components/ChatComponents/UserPart";
 import { FaPaperPlane, FaRegSmile } from "react-icons/fa";
 import { MdPhoto } from "react-icons/md";
 import ChatFeed from "../../assets/Components/ChatComponents/ChatFeed";
 import { ChatContext } from "../../contexts/ChatContext";
+import Picker from '@emoji-mart/react';
 
 const Chat = () => {
-  const {chatPartner} = useContext(ChatContext);
+  const { chatPartner } = useContext(ChatContext);
   const listData = getChatData(); //DUMMY FETCHING CHAT DATA
 
   const [input, setInput] = useState('');
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
+  const addEmoji = (emoji) => {
+    setInput(input + (emoji.native || emoji.unified || ""));
+  };
 
   return (
     <div className="w-full h-full flex gap-x-5">
@@ -26,11 +31,16 @@ const Chat = () => {
         </div>
       </div>
       <div className="right w-[70%] h-full shadow-xl rounded-xl px-7 py-4 flex flex-col justify-between bg-white">
-        <UserPart avatar={chatPartner.profile_picture} name={chatPartner.userName} isActive={chatPartner.isActive || true } lastSeen={chatPartner.lastSeen || 'now'} />
+        <UserPart
+          avatar={chatPartner?.profile_picture}
+          name={chatPartner?.userName}
+          isActive={chatPartner?.isActive || true}
+          lastSeen={chatPartner?.lastSeen || 'now'}
+        />
         <div className="chatFeed h-[70%] overflow-scroll" style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
-          <ChatFeed chatData={listData}/>
+          <ChatFeed chatData={listData} />
         </div>
-        <div className="inputPart flex items-center gap-x-2 mt-4 pt-5 pb-1 border-t-gray-300 border-t-[1px]">
+        <div className="inputPart flex items-center gap-x-2 mt-4 pt-5 pb-1 border-t-gray-300 border-t-[1px] relative">
           <div className="flex items-center bg-[#f1f1f1] rounded-lg flex-grow px-3 py-2">
             <input
               type="text"
@@ -39,19 +49,29 @@ const Chat = () => {
               className="flex-grow bg-transparent outline-none text-sm text-gray-700"
               onChange={(e) => setInput(e.target.value)}
               onKeyDown={e => {
-                if(e.key === 'Enter') {
+                if (e.key === 'Enter') {
                   //sending function
                   console.log('Proceed to send', input);
                 }
               }}
             />
-            <FaRegSmile className="text-black opacity-50 cursor-pointer mx-2" />
-            <MdPhoto className="text-black opacity-50 cursor-pointer" />
+            <FaRegSmile className="text-black opacity-50 cursor-pointer mx-2" onClick={() => setShowEmojiPicker(!showEmojiPicker)}/>
+            {showEmojiPicker && (
+              <div style={{ position: 'absolute', bottom: '60px', right: '60px', zIndex: 10 }}>
+                <Picker onEmojiSelect={addEmoji} />
+              </div>
+            )}
+            <MdPhoto
+              className="text-black opacity-50 cursor-pointer"
+            />
           </div>
-          <button className="w-10 h-9 bg-mainColor rounded-lg flex items-center justify-center text-white cursor-pointer" onClick={() => {
-            //sending function
-            console.log('Proceed to send', input);
-          }}>
+          <button
+            className="w-10 h-9 bg-mainColor rounded-lg flex items-center justify-center text-white cursor-pointer"
+            onClick={() => {
+              //sending function
+              console.log('Proceed to send', input);
+            }}
+          >
             <FaPaperPlane />
           </button>
         </div>
